9f3566afb8c44b51cecb84f8ef6124ebd49f753f
pantheepatel <patel.panthee.18@gmail.com>
2025-02-14 19:35:05 +0530

    methods file added

--- OOPs/MethodsFile.cs
+++ OOPs/MethodsFile.cs
@@ -2,6 +2,18 @@ namespace OOPs
 {
     public class MethodsFile
     {
+        // if not program.cs
+        //public static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Into new Project: OOPs");
+
+        //    // topic - methods
+        //    //public static void Main()
+        //    //static means that the method belongs to the Program class and not an object of the Program class.
+        //    MethodsFile.MyMethod("Panthee", 20); // this passed value is called argument
+        //    MethodsFile.MyMethod(); // use of default parameters
+        //    MethodsFile.MyMethod(age: 16, name: "Nehee Patel");
+        //}
         public static void MyMethod(string name = "user", int age = 0) // name is called parameter of MyMethod
         {
             Console.WriteLine();
